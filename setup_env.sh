4f6838d42f852a0bb850b30de79b411bc64da87b
jhossain <jhossain@lxplus.cern.ch>
2024-04-23 00:03:12 +0200

    some minor changes

--- setup_env.sh
+++ setup_env.sh
@@ -1,3 +1,4 @@
+#export XRD_NETWORKSTACK=IPv4
 export SCRAM_ARCH=slc7_amd64_gcc700
 #!/bin/bash  
 
@@ -5,7 +6,8 @@ cmsenv
 git cms-init
 
 
-voms-proxy-init -voms cms -out ${HOME}/proxy/x509up.px --valid 168:00
-echo X509_USER_PROXY=/afs/cern.ch/user/j/jhossain/proxy/x509up.px
-export X509_USER_PROXY=/afs/cern.ch/user/j/jhossain/proxy/x509up.px
-
+voms-proxy-init -voms cms -out ${HOME}/proxy/x509up_u95987 --valid 168:00
+#voms-proxy-init -voms cms -out ${HOME}/tmp/x509up_u95987 --valid 168:00
+echo X509_USER_PROXY=/afs/cern.ch/user/j/jhossain/proxy/x509up_u95987
+export X509_USER_PROXY=/afs/cern.ch/user/j/jhossain/proxy/x509up_u95987
+#export X509_USER_PROXY=/afs/cern.ch/user/j/jhossain/tmp/x509up_u95987
